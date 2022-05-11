96e2454e4c4251df4b9ba9dd4217858457039c63
Pseudoioannes <pseudoioannes@gmail.com>
2022-05-12 00:44:14 +0600

    52 min

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,14 @@
 from django.http import JsonResponse
+import json
+
+from products.models import Product
 
 def api_home(request):
-    return JsonResponse({"message": "Django api response!"})
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data["title"] = model_data.title
+        data["content"] = model_data.content
+    return JsonResponse(data)
 # Create your views here.
 
