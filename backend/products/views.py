6073f24518b6b61fb2a07644d713ffc0b4c7581d
Pseudoioannes <pseudoioannes@gmail.com>
2022-05-20 18:07:29 +0600

    3:00 min

--- backend/products/views.py
+++ backend/products/views.py
@@ -8,6 +8,8 @@ from .serializers import ProductSerializer
 
 from .permissions import IsStaffEditorPermission
 
+from api.authentication import TokenAuthentication
+
 class ProductMixinView(mixins.ListModelMixin, generics.GenericAPIView):
 
     queryset = Product.objects.all()
@@ -22,7 +24,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     authentication_classes = [authentication.SessionAuthentication,
-    authentication.TokenAuthentication]
+    TokenAuthentication]
     permission_classes = [IsStaffEditorPermission]
 
     def perform_create(self, serializer):
--- py_client/list.py
+++ py_client/list.py
@@ -3,16 +3,17 @@ from getpass import getpass
 
 
 auth_endpoint = "http://127.0.0.1:8000/api/auth/"
+username = input("username:\n")
 password = getpass()
 
-auth_response = requests.get(auth_endpoint, json={"username":"cfe", "password": password})
+auth_response = requests.post(auth_endpoint, json={"username":username, "password": password})
 print(auth_response.json())
 
 
 if auth_response.status_code == 200:
     token = auth_response.json()["token"]
     headers = {
-        "Authorization":f"Token {token}"
+        "Authorization":f"Bearer {token}"
     }
     endpoint = "http://127.0.0.1:8000/api/products/"
 
